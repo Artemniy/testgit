7d06f76f7bf0be446e96d4228030d6790a91a753
Artemniy <shiposhaartem05@gmail.com>
2022-09-22 12:06:54 +0300

    Another one

--- lib/user.dart
+++ lib/user.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/material.dart';
+
 class User {
   User({required this.name, required this.dob, required this.gender});
   final String name;
   final String dob;
   final String gender;
 }
-
